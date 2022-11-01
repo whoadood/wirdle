b045faab06338d79a7bed32f5abddc922e6127b7
Castle <castlerock88@protonmail.com>
2022-11-01 07:22:15 -0400

    birdle baby

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,64 @@
-import { Game } from "./game";
+import { Game } from "./utils/game";
 import chalk from "chalk";
+import inquirer from "inquirer";
 
-export function wordLogger() {
+export async function wordLogger() {
   const newGame = new Game();
-  console.log(newGame);
+  console.log(newGame.answer);
+  guess(newGame);
+}
+
+async function guess(game: Game) {
+  const answers = await inquirer.prompt([
+    {
+      type: "input",
+      name: `guess`,
+      message: "enter your guess",
+      validate(input) {
+        if (input.length !== 5) {
+          console.log(
+            chalk.red(`${chalk.bold(" ERROR")} input must be 5 characters long`)
+          );
+        } else {
+          return true;
+        }
+      },
+    },
+  ]);
+
+  game.guesses.push(answers.guess);
+  game.round++;
+  check(answers.guess, game, () => guess(game));
+}
+
+function check(guess: string, game: Game, callback: () => void) {
+  const guessArr = guess.split("");
+  const answerArr = game.answer.split("");
+  const formatGuess = guessArr
+    .map((letter, index) => {
+      if (answerArr.includes(letter)) {
+        if (letter === answerArr[index]) {
+          return chalk.green(letter);
+        } else {
+          return chalk.cyan(letter);
+        }
+      } else {
+        return letter;
+      }
+    })
+    .join("");
+  console.log(formatGuess);
+  if (game.round > 6) {
+    console.log(
+      chalk.red(`${chalk.bold(game.answer)} you lose, better luck next time!`)
+    );
+    process.exit(0);
+  }
+  if (guess === game.answer) {
+    console.log(
+      chalk.green(`${chalk.bold("CONGRADULATIONS")} you guessed the birdle!`)
+    );
+  } else {
+    callback();
+  }
 }
